ee88094fa92d078033af6ba7d9a2a2083fed8e83
Bruno Costa <brunocosta0427@gmail.com>
2024-01-26 10:30:52 -0300

    Remodeling tables. Submit for further analyses.

--- Base de Dados.sql
+++ Base de Dados.sql
@@ -1,99 +1,90 @@
+
 ----------------------------------------------//---------------------------------------------------------------------
- -- Account
+-- CreditCard
+--Armazena as informaçoes do atributo composto.
+DROP TABLE IF EXISTS CreditCard CASCADE;
+
+CREATE TABLE CreditCard (
+	cardNumber INTEGER PRIMARY KEY,
+	securityCode VARCHAR(3),
+	cardBrand VARCHAR(30),
+	cardName VARCHAR(100)
+);
+
+
+-- Account
+ --Armazena a informação de 1 só cartão atrelado a conta.
+ 
+ 
 DROP TABLE IF EXISTS Account CASCADE;
 
 CREATE TABLE Account (
 	email VARCHAR(80) PRIMARY KEY,
-	_password VARCHAR(25));
-
-
-INSERT INTO Account
-VALUES 
-	('exemplo@gmail.com', 'senha123'),
-	('teste@gmail.com', 'pa55word');
-
-
-SELECT *
-FROM Account;
-
--- Subscription
-
-DROP TABLE IF EXISTS _Subscription CASCADE;
-
-CREATE TABLE _Subscription (
-	signingDate date,
-	paymentStatus VARCHAR(20),
-	durationSub smallint
-);
+	_password VARCHAR(25),
+	cardNumber INTEGER,
+	FOREIGN KEY (cardNumber) REFERENCES CreditCard(cardNumber)
+	ON UPDATE CASCADE 
+	ON DELETE CASCADE	
+	);
+	
 
 -- Plan
+--São os diferentes tipos de planos do Netflix.
+DROP TABLE IF EXISTS _Plan CASCADE;
 
-DROP TABLE IF EXISTS Plan CASCADE;
-
-CREATE TABLE Plan (
-	plan VARCHAR(100) PRIMARY KEY,
+CREATE TABLE _Plan (
+	planID INTEGER PRIMARY KEY,
+	planName VARCHAR(30),
 	fee NUMERIC(5, 2)
 );
 
--- CreditCard
-
-DROP TABLE IF EXISTS CreditCard CASCADE;
-
-CREATE TABLE CreditCard (
-	cardNumber VARCHAR(19) PRIMARY KEY,
-	securityCode VARCHAR(3),
-	cardBrand VARCHAR(30),
-	cardName VARCHAR(100));
 
 
-SELECT *
-FROM CreditCard;
-
--- User
-
-DROP TABLE IF EXISTS _User CASCADE;
-
-CREATE TABLE _User
-	(email VARCHAR(80) REFERENCES ACCOUNT 
-		ON UPDATE CASCADE 
-	 	ON DELETE CASCADE,
-	username VARCHAR(20),
-	 
-	PRIMARY KEY (email, username));
 
 
-INSERT INTO _User
-VALUES  
-	('exemplo@gmail.com', 'Vini'),
-	('exemplo@gmail.com', 'Sara'),
-	('teste@gmail.com', 'Vini');
 
-SELECT *
-FROM _User;
+-- _Subscription
+--Armazena informações tanto da conta que esta atrelada, quanto do tipo de plano que representa.
+--Uma subscription é uma relação entre uma conta, que assinou um plano, em um momento.
+--Uma conta pode ter várias assinaturas desde que em períodos diretentes. Precisa acabar uma para começar outra.
+--Mas mesmo assinaturas ja terminadas continuam sendo armazenadas para fins de histórico.
+DROP TABLE IF EXISTS _Subscription CASCADE;
 
--- Content
-
-DROP TABLE IF EXISTS _Content CASCADE;
-
-CREATE TABLE _Content(
-	contentID varchar (20) PRIMARY KEY,
-	synopsis varchar (1000),
-	title VARCHAR(100),
-	yearProdution timestamp
+CREATE TABLE _Subscription (
+	subcriptionID INTEGER,
+	signingDate date,
+	paymentStatus VARCHAR(20),
+	durationSub smallint,
+	email VARCHAR(80),
+	planID INTEGER,
+	FOREIGN KEY (email) REFERENCES Account(email)
+	ON UPDATE CASCADE 
+	ON DELETE CASCADE,
+	FOREIGN KEY (planID) REFERENCES _Plan(planID)
+	ON UPDATE CASCADE 
+	ON DELETE CASCADE
 );
 
 
-INSERT INTO _Content
-VALUES 
-	('00000000000000000001','aquele filme que o personagem faz aquela coisa','Aquele la','1/12/2022'),
-	('00000000000000000002','o outro que eles fazem outras coisas em outro lugar','O outro','1/1/2022'),
-	('00000000000000000003','','','1/1/2022'),
-	('00000000000000000004','','','1/1/2018'),
-	('00000000000000000005','','','1/1/2017');
+
+-- User
+--É unico dentro de uma conta, porém tem informações próprias sobre ele. Informações de conteúdo assistido, por exemplo.
+DROP TABLE IF EXISTS _User CASCADE;
+
+CREATE TABLE _User
+	(email VARCHAR(80) REFERENCES Account
+		ON UPDATE CASCADE 
+	 	ON DELETE CASCADE,
+	userName VARCHAR(20),
+	 
+	PRIMARY KEY (email, userName));
 
 
-SELECT *
-FROM _Content;
+DROP TABLE IF EXISTS Watches CASCADE;
+
+
+
+
 
 -- Category
 
@@ -103,139 +94,122 @@ CREATE TABLE Category (
 	categoryName VARCHAR(20) PRIMARY KEY);
 
 
-INSERT INTO Category
-VALUES  
-	('ação'),
-	('aventura'),
-	('comedia'),
-	('drama'),
-	('terror');
+
+--Worker 
+--Can be a actor or director, each title has only one director and many actor as much as needed.
+DROP TABLE IF EXISTS Worker ;
+
+CREATE TABLE Worker(
+	workerID INTEGER PRIMARY KEY,
+	oscar BOOLEAN,
+	workerName VARCHAR(50)
+);
 
 
-SELECT *
-FROM Category;
 
---Watches
 
-DROP TABLE IF EXISTS Watches CASCADE;
 
-CREATE TABLE Watches(
-	username VARCHAR(20),
-	email VARCHAR(80),
-	FOREIGN KEY (email, username)
-		REFERENCES _USER 
+DROP TABLE IF EXISTS Title Cascade;
+
+CREATE TABLE Title(
+	titleID INTEGER PRIMARY KEY,
+	yearProd INTEGER,
+	synopsis VARCHAR(250),
+	rating SMALLINT,
+	categoryName VARCHAR(20),
+	director INTEGER,
+	FOREIGN KEY  (director) REFERENCES Worker(workerID)
 		ON UPDATE CASCADE
-		ON DELETE CASCADE,
-	contentID VARCHAR(20) 
-		REFERENCES _Content
-		ON UPDATE CASCADE
-		ON DELETE CASCADE,
+		ON DELETE SET NULL,
+ 	FOREIGN KEY (categoryName) REFERENCES Category(categoryName)
+        ON UPDATE CASCADE
+        ON DELETE SET NULL
 	
-	PRIMARY KEY (username, email, contentID),
-	_timestamp timestamp
-);
-
-INSERT INTO Watches
-VALUES 
-	('Vini', 'teste@gmail.com', '00000000000000000001', '22/12/2023 15:15');
-
-
-SELECT *
-FROM Watches;
-
---Crew
-
-DROP TABLE IF EXISTS Crew CASCADE;
-
-CREATE TABLE Crew(
-	contentID VARCHAR(20) PRIMARY KEY
-		REFERENCES _CONTENT
-		ON UPDATE CASCADE
-		ON DELETE CASCADE,
-	DIRECTOR VARCHAR(100)
-);
-
-SELECT *
-FROM CREW;
-
--- Cast
-
-DROP TABLE IF EXISTS _Cast CASCADE;
-
-CREATE TABLE _Cast(
-	contentID VARCHAR(20) PRIMARY KEY
-		REFERENCES Crew
-		ON UPDATE CASCADE
-		ON DELETE CASCADE);
-	
---Movie
-
-DROP TABLE IF EXISTS Movie CASCADE;
-
-CREATE TABLE Movie(
-	contentID varchar(20) 
-		REFERENCES _Content
-		ON UPDATE CASCADE
-		ON DELETE CASCADE,
-	movieDur TIME
 );
 
 
-SELECT *
-FROM Movie;
+
+
+-- Content
+--This table brings together the entities Movie, Episode and content.
+DROP TABLE IF EXISTS _Content CASCADE;
+
+CREATE TABLE _Content(
+	contentID INTEGER PRIMARY KEY,
+	duration TIME,
+	titleID INTEGER,
+	FOREIGN KEY (titleID) REFERENCES Title(titleID)	
+);
+
+
+
+-- Watches, the relashionship between content and user. 
+CREATE TABLE Watches (
+    email VARCHAR(80),
+    userName VARCHAR(20),
+    contentID INTEGER,
+    stopTime TIME, -- Use TIME type to store only hour and minute
+	PRIMARY KEY (email, userName, contentID),
+    FOREIGN KEY (email, userName) REFERENCES _User(email, userName)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (contentID) REFERENCES _Content(contentID)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+
+
+
+
+--Acts
+
+DROP TABLE IF EXISTS Acts CASCADE;
+
+CREATE TABLE Acts (
+	workerID INTEGER,
+	titleID INTEGER,
+	PRIMARY KEY (titleID, workerID),
+    FOREIGN KEY (titleID) REFERENCES Title(titleID)
+	ON UPDATE CASCADE
+	ON DELETE CASCADE,
+    FOREIGN KEY (workerID) REFERENCES Worker(workerID)
+	ON UPDATE CASCADE
+	ON DELETE CASCADE
+);
+
+
+
+
 
 -- Series
 
 DROP TABLE IF EXISTS Series CASCADE;
 
-CREATE TABLE SERIES(
-	contentID varchar(20) PRIMARY KEY
-		REFERENCES _Content
+CREATE TABLE Series(
+	seriesID INTEGER PRIMARY KEY
+		REFERENCES Title(titleID)
 		ON UPDATE CASCADE
 		ON DELETE CASCADE,
-	numSeasons smallint
+	numSeasons SMALLINT
 );
 
 
-SELECT *
-FROM SERIES;
+--Belongs
+--relacionamento de pertencimento de episódios a uma série
+DROP TABLE IF EXISTS Belongs;
 
---Season
-
-DROP TABLE IF EXISTS Season CASCADE;
-
-CREATE TABLE Season(
-	contentID VARCHAR(20)
-		REFERENCES Series
-		ON UPDATE CASCADE 
-		ON DELETE CASCADE,
-	SEASONID smallint,
-	PRIMARY KEY (contentID,	seasonID),
-	numEpisodes int
-);
-
-
-SELECT *
-FROM Season;
-
---Episode
-
-DROP TABLE IF EXISTS Episode CASCADE;
-
-CREATE TABLE Episode(
-	episodeID smallint,
-	contentID VARCHAR(20),
-	seasonID smallint,
-	FOREIGN KEY (contentID, SEASONID)
-		REFERENCES SEASON
+CREATE 	TABLE Belongs(
+	seriesID INTEGER,
+	episodeID INTEGER,
+	season SMALLINT,
+	
+	PRIMARY KEY (seriesID, episodeID),
+	FOREIGN KEY (episodeID)	REFERENCES _Content(contentID)
 		ON UPDATE CASCADE
-		ON DELETE CASCADE,
-	episodeDur TIME,
-	episodeTitle VARCHAR(50),
-	description VARCHAR(200),
-	PRIMARY KEY (contentID, seasonID, episodeID)
+		ON DELETE CASCADE,	
+	FOREIGN KEY (seriesID)	REFERENCES Title(titleID)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
+	
 );
-
-
-SELECT *
-FROM Episode;
